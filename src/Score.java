bb22895cb31011434c97dcdfbdee13fa89be443f
TrevisGreen <Trevis.Green@gmail.com>
2023-02-07 03:14:29 -0600

    created methods and variables that maybe needed for all the classes

--- src/Score.java
+++ src/Score.java
@@ -1,2 +1,20 @@
-public class Score {
+import  java.awt.*;
+import  java.awt.event.*;
+import  java.util.*;
+import  javax.swing.*;
+
+public class Score extends Rectangle {
+
+    static int GAME_WIDTH;
+    static int GAME_HEIGHT;
+    int player1;
+    int player2;
+
+    Score() {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
 }
